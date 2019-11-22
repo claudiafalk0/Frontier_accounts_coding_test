0391c5ee32305079f80b8ff22dae222a7e44c07a
Claudia Falk <claudiafalk0@gmail.com>
2019-11-22 16:06:07 -0700

    data is displaying in each column

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "./home.css";
 
 
 // const Accounts = props => (
@@ -37,21 +38,24 @@ export default class Home extends Component {
 
 
     activeData() {
-        return this.state.Accounts.map(function (currentAccount) {
-           if(currentAccount.AccountStatusId === 2){
-               overdueAccts.push(currentAccount);
-               return console.log(overdueAccts)
-           }else if(currentAccount.AccountStatusId === 1){
-               return inactiveAccts.push(currentAccount)
-           }else{
-               return activeAccts.push(currentAccount)
-           }
-        })
+           this.state.Accounts.map(function(currentAccount) {
+            if (currentAccount.AccountStatusId === 2) {
+                overdueAccts.push(currentAccount);
+                return console.log(overdueAccts)
+            } else if (currentAccount.AccountStatusId === 1) {
+                inactiveAccts.push(currentAccount)
+                return console.log(inactiveAccts) 
+            } else {
+                activeAccts.push(currentAccount)
+                return console.log(activeAccts)
+            }
+           })
     }
 
     render() {
         return (
             <div>
+            {this.activeData()}
                 <article className="grid">
                     <header className="grid">
                         <div className="title-container">
@@ -63,36 +67,42 @@ export default class Home extends Component {
                             <h2>Accounts</h2>
                         </div>
                         <section className="grid" id="account-grid">
-                            {this.activeData()}
-                        <section className="account-column grid" id="active-account-column">
+                            
+                            <section className="account-column grid" id="active-account-column">
                                 <div className="account-container-title" id="active-account-container-title">
-                                    
+
                                     <h3>Active</h3>
                                     {/* if AccountStatusID === 2, display here */}
                                 </div>
                                 <div className="account-container overdue-account">
                                     <ul className="account-data-list">
-                                        <li><label>Name:</label>{activeAccts.LastName}, {activeAccts.FirstName}</li>
-                                        <li><label>Email:</label>{activeAccts.Email}</li>
-                                        <li><label>Phone Number:</label>{activeAccts.PhoneNumber}</li>
-                                        <li><label>Amount Due:</label>{activeAccts.AmountDue}</li>
-                                        <li><label>Due Date:</label>{activeAccts.DueDate}</li>
+                                        {activeAccts.map(function(elem) {
+                                            return <> <li key={elem.id}> <label>Name:</label>  {elem.LastName}, {elem.FirstName} </li>
+                                             <li key={elem.id}> <label>Email:</label> {elem.Email}</li>
+                                            <li key={elem.id}> <label>Phone Number:</label> {elem.PhoneNumber}</li>
+                                            <li key={elem.id}> <label>Amount Due:</label>{elem.AmountDue}</li>
+                                            <li key={elem.id}> <label>Due Date:</label>{elem.DueDate}</li>
+                                            </>
+                                        })}
                                     </ul>
                                 </div>
                             </section>
                             <section className="account-column grid" id="overdue-account-column">
                                 <div className="account-container-title" id="overdue-account-container-title">
-                                    
+
                                     <h3>Overdue</h3>
                                     {/* if AccountStatusID === 2, display here */}
                                 </div>
                                 <div className="account-container overdue-account">
                                     <ul className="account-data-list">
-                                        <li><label>Name:</label>{overdueAccts.LastName}, {overdueAccts.FirstName}</li>
-        <li><label>Email:</label>{overdueAccts.Email}</li>
-        <li><label>Phone Number:</label>{overdueAccts.PhoneNumber}</li>
-        <li><label>Amount Due:</label>{overdueAccts.AmountDue}</li>
-        <li><label>Due Date:</label>{overdueAccts.DueDate}</li>
+                                    {overdueAccts.map(function(elem) {
+                                             return <> <li key={elem.id}> <label>Name:</label>  {elem.LastName}, {elem.FirstName} </li>
+                                             <li key={elem.id}> <label>Email:</label> {elem.Email}</li>
+                                            <li key={elem.id}> <label>Phone Number:</label> {elem.PhoneNumber}</li>
+                                            <li key={elem.id}> <label>Amount Due:</label>{elem.AmountDue}</li>
+                                            <li key={elem.id}> <label>Due Date:</label>{elem.DueDate}</li>
+                                            </>
+                                        })}
                                     </ul>
                                 </div>
                             </section>
@@ -103,11 +113,14 @@ export default class Home extends Component {
                                 </div>
                                 <div className="account-container inactive-account">
                                     <ul className="account-data-list">
-                                        <li><label>Name:</label>{overdueAccts.LastName}, {overdueAccts.FirstName}</li>
-                                        <li><label>Email:</label>{inactiveAccts.Email}</li>
-                                        <li><label>Phone Number:</label>{inactiveAccts.PhoneNumber}</li>
-                                        <li><label>Amount Due:</label>{inactiveAccts.AmountDue}</li>
-                                        <li><label>Due Date:</label>{inactiveAccts.DueDate}</li>
+                                    {inactiveAccts.map(function(elem) {
+                                             return <> <li key={elem.id}> <label>Name:</label>  {elem.LastName}, {elem.FirstName} </li>
+                                             <li key={elem.id}> <label>Email:</label> {elem.Email}</li>
+                                            <li key={elem.id}> <label>Phone Number:</label> {elem.PhoneNumber}</li>
+                                            <li key={elem.id}> <label>Amount Due:</label>{elem.AmountDue}</li>
+                                            <li key={elem.id}> <label>Due Date:</label>{elem.DueDate}</li>
+                                            </>
+                                        })}
                                     </ul>
                                 </div>
                             </div>
